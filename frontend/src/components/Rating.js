465996bef215fc0cb7256ca97a3fe59a24de6979
sakthi s <velsakthis5301@gmail.com>
2021-12-29 16:57:36 +0530

    first commit

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Rating(props) {
+    const { rating, numReviews } = props;
+    return (
+        <div className="rating">
+        <span>  
+            <i className={ rating >=1?"fa fa-star": rating >=0.5?'fa fa-star-half-0':'fa fa-star-0'}></i> </span>
+        <span>  <i className={ rating >=2?"fa fa-star": rating >=1.5?'fa fa-star-half-0':'fa fa-star-0'}></i> </span>
+        <span>   <i className={ rating >=3?"fa fa-star": rating >=2.5?'fa fa-star-half-0':'fa fa-star-0'}></i> </span>
+        <span>   <i className={ rating >=1?"fa fa-star": rating >=3.5?'fa fa-star-half-0':'fa fa-star-0'}></i> </span>
+        <span>  <i className={ rating >=5?"fa fa-star": rating >=4.5?'fa fa-star-half-0':'fa fa-star-0'}></i> </span>
+   <span>
+       {numReviews + ' reviews'}
+   </span>
+    </div>
+    );
+}
